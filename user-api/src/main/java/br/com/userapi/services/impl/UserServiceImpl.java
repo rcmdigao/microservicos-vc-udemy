c64d034da22571ae3ee1547abfe90f5e26ae27ed
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2023-01-23 11:53:26 -0300

    Load Balancer e Api Gateway

--- user-api/src/main/java/br/com/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/userapi/services/impl/UserServiceImpl.java
@@ -5,18 +5,23 @@ import br.com.userapi.reporitories.UserRepository;
 import br.com.userapi.services.UserService;
 import br.com.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final Environment env;
     private final UserRepository repository;
 
     @Override
     public User findById(Long id) {
+        log.info("PAYROLL_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return repository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Objeto não encotrado!!!"));
     }
